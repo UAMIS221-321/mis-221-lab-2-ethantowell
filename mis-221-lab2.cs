970feaefa25d1ea746e2a944b157b59d1f6bbd48
eltowell@crimson.ua.edu <eltowell@crimson.ua.edu>
2024-09-08 21:30:13 -0500

    Update mis-221-lab2.cs

--- mis-221-lab2.cs
+++ mis-221-lab2.cs
@@ -11,10 +11,10 @@ const double COST_OF_TOPPING = 0.55;
 const double DISCOUNT_AMOUNT = 0.10;
 // Prompt user for sandwiches
 System.Console.WriteLine("How many sandwiches do you want?");
-numberOfSandwiches= int.Parse(Console.Readline());
+numberOfSandwiches = int.Parse(Console.Readline());
 System.Console.WriteLine("Sandwiches "+numberOfSandwiches);
 //Prompt user for toppings
-System.Console.Writeline("How many toppings do you want?");
+System.Console.WriteLine("How many toppings do you want?");
 numberOfToppings = int.Parse(Console.ReadLine());
 System.Console.WriteLine("Toppings "+numberOfToppings);
 System.Console.WriteLine("Would you like to add a tip?");
@@ -22,6 +22,5 @@ tip = double.Parse(Console.ReadLine());
 double totalSandwichCost = COST_OF_SANDWICH * numberOfSandwiches;
 double totalToppingCost = COST_OF_TOPPING * numberOfToppings;
 double baseCost = totalSandwichCost + totalToppingCost;
-
 orderCost = tip + baseCost * (1-DISCOUNT_AMOUNT);
 System.Console.WriteLine(orderCost);
